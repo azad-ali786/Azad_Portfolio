199dc05aeae849c42b3c266e4ce642187a9d3121
unknown <azad_ug@cse.nits.ac.in>
2020-12-28 19:58:28 +0530

    Change in Page3.js

--- src/components/page3.js
+++ src/components/page3.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
 import img from "../images/pic2.png";
 const Page3 = () => {
   return (
