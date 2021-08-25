454bc1567a6726998101dfae7d0d536689d17d12
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-08-25 14:36:16 +0530

    Cursor Speed

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -3,7 +3,7 @@ import { motion, useMotionValue, useSpring } from "framer-motion"
 const Cursor = () => {
     const cursorX = useMotionValue(-100);
 const cursorY = useMotionValue(-100);
-const springConfig = { damping: 25, stiffness: 200 };
+const springConfig = { damping: 25, stiffness: 700 };
 const cursorXSpring = useSpring(cursorX, springConfig);
 const cursorYSpring = useSpring(cursorY, springConfig);
   useEffect(() => {
